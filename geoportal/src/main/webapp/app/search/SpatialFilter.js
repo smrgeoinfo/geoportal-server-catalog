29b37f45e10c46f65517543af815e7a425866791
Stephen Richard <steve.richard@ldeo.columbia.edu>
2017-12-05 09:02:09 -0500

    comment out code that makes all searches centroid search
    per https://github.com/Esri/geoportal-server-catalog/issues/104

--- geoportal/src/main/webapp/app/search/SpatialFilter.js
+++ geoportal/src/main/webapp/app/search/SpatialFilter.js
@@ -403,19 +403,19 @@ function(declare, lang, array, aspect, djQuery, on, domConstruct, domClass, domG
         var qry = {"geo_shape":{}};
         qry.geo_shape[field] = {"shape":shp,"relation":relation};
         
-        // TODO
-        qry = {"geo_bounding_box": {
-          "envelope_cen_pt" : {
-            "top_left" : {
-              "lat" : env.ymax,
-              "lon" : env.xmin
-            },
-            "bottom_right" : {
-              "lat" : env.ymin,
-              "lon" : env.xmax
-            }
-          }
-        }};
+        // SMR comment out per https://github.com/Esri/geoportal-server-catalog/issues/104
+//        qry = {"geo_bounding_box": {
+//          "envelope_cen_pt" : {
+//            "top_left" : {
+//              "lat" : env.ymax,
+//              "lon" : env.xmin
+//            },
+//            "bottom_right" : {
+//              "lat" : env.ymin,
+//              "lon" : env.xmax
+//            }
+//          }
+//        }};
        
         
         return qry;
